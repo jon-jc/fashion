75cea89598263d566b2bc7394cd1a2a8e8fc5eb4
jon-jc <jonathancho.jc@gmail.com>
2024-06-12 14:21:12 -0700

    Add header and footer components to about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,10 +1,14 @@
 import About from "@/components/about";
+import FooterPage from "@/components/footer";
+import Header from "@/components/header";
 import React from "react";
 
 const page = () => {
   return (
     <div>
+      <Header />
       <About />
+      <FooterPage />
     </div>
   );
 };
